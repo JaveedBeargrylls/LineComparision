30272b1be93125f78ca20dfeeacafa4120f98f42
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-06-09 18:02:13 +0530

    [Javeed] modified to the two line comparisions

--- LineComparision.java
+++ LineComparision.java
@@ -2,15 +2,26 @@ public class LineComparision{
 public static void main(String[] args){
 System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
 
-// inserted the variables and logic
+// inserted the variables and logic for two lines and comparision
 
 int x_1 = 10, x_2=15, y_1=20, y_2=25;
-int v_x,v_y;
+int a_1 = 50, a_2=55, b_1=60, b_2=75;
+int v_x,v_y,v_a,v_b;
 v_x = x_2 - x_1;
 v_y = y_2 - y_1;
+v_a = a_2 - a_1;
+v_b = b_2 - b_1;
 double a = Math.pow(v_x,2);
 double b = Math.pow(v_y,2);
-double length = Math.sqrt(a+b);
-System.out.println("length of a line is : "+length);
+double c = Math.pow(v_a,2);
+double d = Math.pow(v_b,2);
+double length_1 = Math.sqrt(a+b);
+double length_2 = Math.sqrt(c+d);
+System.out.println("length of a first line is : "+length_1);
+System.out.println("length of a second line is : "+length_2);
+if ( length_1 == length_2 )
+System.out.println("the lengths are equal");
+else
+System.out.println("the lengths are not equal");
 }
 }
